9dddced26335b1811fd14c52bf9fadcc31660afd
Asus <louisfuego03@gmail.com>
2022-04-28 20:05:15 +0800

    Added a functioning map in the map tab Might be changed later

--- app/src/main/java/com/example/group4_capstone/Map.java
+++ app/src/main/java/com/example/group4_capstone/Map.java
@@ -2,11 +2,20 @@ package com.example.group4_capstone;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.*;
 
 
@@ -20,6 +29,35 @@ public class Map extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                LatLng latLng1 = new LatLng(7.063078235880636, 125.59600184708944);
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        latLng1, 19
+                ));
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng1);
+                markerOptions.title("MCM");
+
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng1);
+                        markerOptions.title(latLng1.latitude + " : " + latLng1.longitude);
+
+                        googleMap.clear();
+                        googleMap.addMarker(markerOptions);
+                    }
+                });
+            }
+        });
+        return view;
     }
 }
