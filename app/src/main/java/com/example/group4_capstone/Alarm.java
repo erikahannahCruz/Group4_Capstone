2eed4225d7cff63e476ad763e2dabeb51fb03c82
p1p1pupu <11erikacruz11@gmail.com>
2022-04-28 17:09:51 +0800

    Created new fragment Alarm and linked it to floating action button

---
+++ app/src/main/java/com/example/group4_capstone/Alarm.java
@@ -0,0 +1,24 @@
+package com.example.group4_capstone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Alarm extends Fragment {
+
+
+    public Alarm() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alarm_set, container, false);
+    }
+}
--- app/src/main/java/com/example/group4_capstone/MainActivity.java
+++ app/src/main/java/com/example/group4_capstone/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.group4_capstone;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
 
     BottomNavigationView bottomNavigationView;
     TopBanner topbanner = new TopBanner();
+    FloatingActionButton fabAlarm;
+
+    Profile profile = new Profile();
+    Map map = new Map();
+    Schedule schedule = new Schedule();
+    Settings settings = new Settings();
+    Alarm alarmSet = new Alarm();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         catch (NullPointerException e) {
         }
 
+        //sets up bottom navigation and selects it on Map upon startup
         bottomNavigationView = findViewById(R.id.bottomNavigationView) ;
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.Map);
@@ -33,12 +44,21 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         //displays top banner information on startup
         getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentTopBanner, topbanner).commit();
 
-    }
+        fabAlarm = findViewById(R.id.fabAlarm);
 
-    Profile profile = new Profile();
-    Map map = new Map();
-    Schedule schedule = new Schedule();
-    Settings settings = new Settings();
+        fabAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentMain, alarmSet).commit();
+
+                /* Intent intent = new Intent(MainActivity.this,Alarm.class);
+                startActivity(intent); */
+            }
+
+        });
+
+
+    }
 
 
     @Override
@@ -63,5 +83,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
        }
         return false;
     }
+
+
+
 
 }
